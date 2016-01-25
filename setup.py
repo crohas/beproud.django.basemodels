2413c7444e59f162a190a8ba7a3d3492e63b5584
crohas <yasuyuki.kato@beproud.jp>
2016-01-25 15:21:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ requires = [
 setup(
     name="beproud.django.basemodels",
     version="0.0.0",
+    author='BeProud Inc.',
+    author_email='project@beproud.jp',
     packages=find_packages(),
     namespace_packages=[
         "beproud",
